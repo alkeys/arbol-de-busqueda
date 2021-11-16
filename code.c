f182c8b036fdcf7cb1cd4676ae3527d756f6a5ce
alex <alex@Avi.localdomain>
2021-11-15 23:05:26 -0600

    arbol 0.0.3

--- code.c
+++ code.c
@@ -12,11 +12,6 @@ Arbol *nuevoNodo(dato Dato)
 
 void insertar(Arbol *nodo, int dato)
 {
-    if (nodo == NULL)
-    {
-        nodo = nuevoNodo(dato);
-        return 0;
-    }
     //si es mallor se ira ala deracha xD
     if (nodo->Dato < dato)
     {
@@ -36,4 +31,65 @@ void insertar(Arbol *nodo, int dato)
             insertar(nodo->izq,dato);
         }
     }
+}
+
+
+void mostrar (Arbol *arbolNodo)
+{
+  if (arbolNodo == NULL)
+    {
+      return;
+    }
+  printf (" %d", arbolNodo->Dato);
+  mostrar (arbolNodo->der);
+  mostrar (arbolNodo->izq);
+}
+
+
+
+void borrarArbol (Arbol *raiz){
+    if (*raiz != NULL){
+        borrarArbol (&(*raiz)->izq);
+        borrarArbol (&(*raiz)->der);
+        free (*raiz);
+        *raiz = NULL;
+    }
+}
+
+Arbol buscar (Arbol raiz, int dato){
+    if (raiz == NULL){
+        return NULL;
+    }else{
+        if (dato == raiz->Dato){
+            return raiz;
+        }else{
+            if (dato < raiz->Dato){
+                return buscar (raiz->izq, dato);
+            }else{
+                return buscar (raiz->der, dato);
+            }
+        }
+    }
+}
+
+
+int alturaArbol(Arbol raiz, int dato){
+    int H=0;
+    Arbol arbolAux=raiz;
+    while (arbolAux!=NULL){
+        if (dato == arbolAux->Dato){           
+            return H;
+        }else{
+            H++;
+            if (dato < arbolAux->Dato){               
+                arbolAux=arbolAux->izq;
+            }else{
+                if (dato > arbolAux->Dato){               
+                    arbolAux=arbolAux->der;
+                }
+            }
+        }
+    }
+printf("\nno se encontro el valor\n");
+return 0;
 }
--- main.c
+++ main.c
@@ -1,23 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
- */
-
-/* 
- * File:   main.c
- * Author: alex
- *
- * Created on 15 de noviembre de 2021, 16:03
- */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "head.h"
+
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+   Arbol *mi_arbol;
+   mi_arbol=nuevoNodo(9);
+   insertar(mi_arbol,10);
+   insertar(mi_arbol,11);
+   insertar(mi_arbol,12);
+   
+   mostrar(mi_arbol);
+    
     return (EXIT_SUCCESS);
 }
 
